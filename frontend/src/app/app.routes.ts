7e93cc14d279b655839b27ef0de95942ec7958b4
agustinmend <jose.mendoza.u1@ucb.edu.bo>
2026-03-06 14:07:58 -0400

    limpieza

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { AgendaComponent } from './features/agenda/agenda';
 
-export const routes: Routes = [
-  { path: '', component: AgendaComponent },
-  { path: '**', redirectTo: '' }
-];
+export const routes: Routes = [];
--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://ubscjoigadsctakxigvv.supabase.co',
-  supabaseKey: 'sb_publishable_bU1YErB1FtFxa5F_rWsJ-w_CktJkV7q'
-};
